bd66a976096c6621f1b3b5563173795807c84b41
micey969 <micey_969@hotmail.com>
2018-10-15 00:34:58 -0400

    fix(live-chat): fix live-chat to show up on homepage

--- main.js
+++ main.js
@@ -1,51 +1,4 @@
-var modal = document.getElementById("my_nav");
-var btn = document.getElementById("hamburger");
-var display = document.getElementsByClassName("navi")[0];
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function() {
-    modal.style.display = "block";
-    if (modal.className === "main_nav") {
-        modal.className = "modal";
-        display.className = "mobile_nav";
-    } 
-}
-span.onclick = function() {
-    modal.style.display = "none";
-    modal.className = "main_nav";
-    display.className = "navi";
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-        modal.className = "main_nav";
-        display.className = "navi";
-    }
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    slides[slideIndex-1].style.display = "block";  
-}
-
-
+//live-chat
 var element = $('.floating-chat');
 setTimeout(function() {
     element.addClass('enter');
@@ -109,3 +62,53 @@ function onMetaAndEnter(event) {
         sendNewMessage();
     }
 }
+
+//mobile-menu
+var modal = document.getElementById("my_nav");
+var btn = document.getElementById("hamburger");
+var display = document.getElementsByClassName("navi")[0];
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+    if (modal.className === "main_nav") {
+        modal.className = "modal";
+        display.className = "mobile_nav";
+    } 
+}
+span.onclick = function() {
+    modal.style.display = "none";
+    modal.className = "main_nav";
+    display.className = "navi";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        modal.className = "main_nav";
+        display.className = "navi";
+    }
+}
+
+//slideshow
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex-1].style.display = "block";  
+}
+
